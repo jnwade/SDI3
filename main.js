f7d848f03ecdce56156433328f4ffcb0cd714873
Jonathan Wade <jnwade@fullsail.edu>
2012-05-16 16:17:24 -0400

    Werkin on it

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-//var handleData = function (json) {
-//    //for (var i = 0; i < json.venue.length; i++) {
-//    //    var venue = json.venue[i];
-//    //    console.log( "Venue: " + venue.name + ", Pay: " + venue.pay + ", Bar Tab: " + venue.barTab );
-//    console.log(json, json2, clubContact);
-//};
-//
-//
-//handleData(json, json2, clubContact);
-
-
-//var jsonstring = JSON.stringify(json);
-//console.log(jsonstring);
-//
-//var receivedjson = JSON.parse(jsonstring);
-//console.log(receivedjson);
-
+var handleData = function (json, json2, clubContact) {
+    console.log(json, json2, clubContact);
+};
 
 // Constructor
 var venue = function (name) {
@@ -26,8 +12,9 @@ var venue = function (name) {
         } else { console.log( "I'm sorry " + guests + ", 18 and up only.")
         };
     };
-     var staffNeeded = function (slammed) {
+     var staffNeeded = function (slammed, needed) {
         if ( slammed === true) {
+            staff.push(needed)
             console.log("Call in reinforcements!");
         } else {console.log( "We got this!")
         };
@@ -39,12 +26,12 @@ var venue = function (name) {
         };
     var getName = function () { return name; };
     return {
-        "name": getName, // Gets the name of the venue
-        "Admission" : ofAgeGuests, // Returns guest names that met the age requirements to enter the venue
+        "name": getName, // Gets the name of the venue (method)
+        "Admission" : ofAgeGuests, 
         "staffNeeded" : staffNeeded, // returns whether or not more staff is needed to accomidate guests
-        "seatGuest" : seatGuest,
-        "guestList" : guestList,
-        "staff" : staff
+        "seatGuest" : seatGuest, // Checks that guests are of age and if so, adds them to the list
+        "guestList" : guestList, // Shows who is currently on the guest list
+        "staff" : staff // Allows us to push additional staff to the staff array
     };
 };
 
@@ -54,7 +41,7 @@ var dexters = venue("Dexters");
 var backStage = venue("Back Stage");
 
 
-
+handleData(json, json2, clubContact);
 dexters.seatGuest("Stephanie", 18); 
 dexters.seatGuest("Mike", 25);
 backStage.seatGuest("Amanda", 20);
@@ -62,13 +49,18 @@ backStage.seatGuest("Josh", 26);
 backStage.seatGuest("Tim", 17);
 backStage.seatGuest("Jenny", 15);
 console.log(dexters.name()); // Accessor
-console.log(dexters);
 dexters.staffNeeded( true, 2);
+console.log(dexters);
 dexters.guestList();
+backStage.staff.push(2);
 console.log(backStage.name());
 console.log(backStage);
 backStage.staffNeeded( false, 0);
 backStage.guestList();
+
+
+
+
 
 
 
